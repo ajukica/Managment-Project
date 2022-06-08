19a91bda8ca4be6063958828396d838a71a99bf6
Ante Jukica <ajukica@gmail.com>
2022-06-08 15:55:59 +0200

    dodan GET zahtjev

--- routes/courses.js
+++ routes/courses.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
+/**
+ * @route Type("post")
+ * Create a course
+ */
 router.post("/create", (req,res) => {
     const {naziv} = req.body;
 
@@ -15,8 +19,16 @@ router.post("/create", (req,res) => {
         if (err) return res.status(400).json({msg: "Neuspjesno umetanje"});
 
         return res.status(200).json({data});
-    })
+    });
     
-})
+});
+
+router.get("/",(req,res)=>{
+    let sql = `SELECT * FROM courses`;
+
+    db.query(sql,(err,result)=>{
+        return res.status(200).json(result);
+    });
+});
 
 module.exports = router;
